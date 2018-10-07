753a8f34b846d56142a44711fc800d1f19fc1bd7
ScarletMcLearn <ratul_shams@yahoo.com>
2018-10-07 21:52:39 +0600

    Update LazyWalker.js

--- LazyWalker/LazyWalker.js
+++ LazyWalker/LazyWalker.js
@@ -36,6 +36,10 @@ myFunction();
 
 
 
+
+// https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
+
+
 function download(filename, text) {
   var element = document.createElement('a');
   element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
