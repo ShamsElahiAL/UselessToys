763e79ce030c666b99d3383b7a1ac39161f801d1
Scarlet McLearn <ratul_shams@yahoo.com>
2020-06-15 07:19:02 +0600

    Update RemovePendingFriends.js

--- RemovePendingFriends/RemovePendingFriends.js
+++ RemovePendingFriends/RemovePendingFriends.js
@@ -1,2 +1,4 @@
+// https://m.facebook.com/friends/center/requests/outgoing/
+
 var inputs = document.getElementsByClassName('_54k8 _56bs _56bt'); 
 for(var i=0; i<inputs.length;i++) { inputs[i].click(); }
